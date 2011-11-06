805d37d70774a92ff7dd7459e15c59f80c2e52e1
Hugo Sena Ribeiro <hugosenari@gmail.com>
2011-11-06 02:03:56 -0200

    adding setup

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+'''
+Created on Nov 6, 2011
+
+@author: hugosenari
+'''
+
+
+from distutils.core import setup
+
+setup(name='mprs2',
+      version='0.9',
+      description='Python mpris2 definition',
+      author='hugosenari',
+      author_email='hugosenari@gmail.com',
+      url='https://github.com/hugosenari/mpris2',
+      packages=['mpris2'],
+     )
