962ea1c1e03bcdb6932abbb1334c95ca8af49775
hugosenari <hugosenari@gmail.com>
2015-11-15 06:15:34 -0200

    use functions of decorator.utils

--- mpris2/utils.py
+++ mpris2/utils.py
@@ -2,36 +2,20 @@
 utils functions not defined in espec
 '''
 
-import dbus, re
+import re
 
 from .interfaces import Interfaces
+from .decorator.utils import get_session, get_uri, get_mainloop  # @UnusedImport
+from .decorator.utils import list_interfaces as _list_interfaces
+from .decorator.utils import implements as _implements
 
-def _match_players_uri(name, pattern='.+'):
-    '''
-        Filter logic for get_players and get_player_uri
-        @param name: string name to test
-        @param pattern=None:  string RegEx to test
-        @return: boolean
-    '''
-    return \
-        re.match('org.mpris.MediaPlayer2', name)\
-            and re.match(pattern, name)
-
-def get_session():
-    '''
-        @return: dbus.SessionBus.get_session()
-    '''
-    return dbus.SessionBus.get_session()
-
-def get_players_uri(pattern='.'):
+def get_players_uri(pattern=''):
     '''
         Return string of player bus name
         @param pattern=None: string RegEx that filter response
         @return: array string of players bus name
     '''
-    for item in get_session().list_names():
-        if _match_players_uri(item, pattern):
-            yield item
+    return get_uri(Interfaces.MEDIA_PLAYER + '.*' + pattern)
 
 
 def get_player_id_from_uri(uri):
@@ -51,79 +35,14 @@ def get_players_id(pattern=None):
         @param pattern=None: string RegEx that filter response
         @return: array string of players bus name
     '''
-    for item in get_session().list_names():
-        if _match_players_uri(item, pattern):
-            yield get_player_id_from_uri(item)
-
-def get_mainloop():
-    try:
-        from dbus.mainloop.glib import DBusGMainLoop
-        DBusGMainLoop(set_as_default=True)
-        try:
-            import gobject
-            return gobject.MainLoop()
-        except:
-            pass
-         
-        try:
-            import gi.repository.GLib
-            return gi.repository.GLib.MainLoop()
-        except:
-            pass
-    except:
-        pass
-    
-    class PyQtMainLoop():
-        def __init__(self, app):
-            self.app = app
-        
-        def run(self):
-            if hasattr(self.app, 'exec_'):
-                self.app.exec_()
-            if hasattr(self.app, 'exec'):
-                method = getattr(self.app, 'exec')
-                method(self.app)
-
-    
-    try:
-        from dbus.mainloop.qt import DBusQtMainLoop
-        DBusQtMainLoop(set_as_default=True)
-        try:
-            from PySide.QtGui import QApplication
-            return PyQtMainLoop(QApplication([]))
-        except:
-            pass
-    
-        try:
-            from PyQt5.QtWidgets import QApplication  
-            return PyQtMainLoop(QApplication([]))
-        except:
-            pass
-    
-        try:
-            from PyQt4 import Qt
-            return PyQtMainLoop(Qt.QApplication([]))
-        except:
-            pass
-    except:
-        pass
-
-    return None
+    for item in get_uri(Interfaces.MEDIA_PLAYER + '.*' + pattern):
+        yield get_player_id_from_uri(item)
 
 def list_interfaces(uri, path=None, bus=None):
-    path = path or Interfaces.OBJECT_PATH
-    import xml.etree.ElementTree as ET
-    
-    bus = bus or dbus.SessionBus.get_session()
-    obj = bus.get_object(uri, path)
-    node = ET.fromstring(obj.Introspect(dbus_interface='org.freedesktop.DBus.Introspectable'))    
-    for interface in node.findall('interface'):
-        yield interface.attrib['name']
+    return _list_interfaces(uri, path or Interfaces.OBJECT_PATH, bus)
         
 def implements(uri, interface, path=Interfaces.OBJECT_PATH, bus=None):
-    for iface in list_interfaces(uri, path, bus):
-        if iface == interface:
-            return True
+    return _implements(uri, interface, path or Interfaces.OBJECT_PATH, bus)
 
 
 if __name__ == '__main__':
